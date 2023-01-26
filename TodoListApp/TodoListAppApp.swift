db0d1d70c6a13657a2fb3538f81071f7e3089ebc
Alexandru-Sergiu Marton <brown121407@posteo.ro>
2023-01-26 20:08:02 +0200

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Alexandru-Sergiu Marton on 26.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
