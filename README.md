9cd01ef04388db7528f4fd929749597147d2f6b6
Alexandru-Sergiu Marton <brown121407@posteo.ro>
2023-01-26 20:39:04 +0200

    Add app code and README.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple To Do List with SwiftUI
+
+No persistence. Simplest thing there can be. From
+[this](https://www.youtube.com/watch?v=z9WHVeam_x0) tutorial.
+
+![A screenshot of the app](./todo-list.png)
--- TodoListApp/ContentView.swift
+++ TodoListApp/ContentView.swift
@@ -7,15 +7,86 @@
 
 import SwiftUI
 
+struct Task: Identifiable {
+    let id = UUID()
+    let name: String
+    var isCompleted: Bool = false
+}
+
+enum Sections: String, CaseIterable {
+    case pending = "Pending"
+    case completed = "Completed"
+}
+
 struct ContentView: View {
+    @State private var tasks = [
+        Task(name: "Mow the lawn", isCompleted: true),
+        Task(name: "Wash the car"),
+        Task(name: "Feed the rabbit")
+    ]
+    @State private var newTaskText: String = ""
+    
+    var pendingTasks: [Binding<Task>] {
+        $tasks.filter { !$0.isCompleted.wrappedValue }
+    }
+    
+    var completedTasks: [Binding<Task>] {
+        $tasks.filter { $0.isCompleted.wrappedValue }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("To Do List")
+                .font(.title)
+            List {
+                ForEach(Sections.allCases, id: \.self) { section in
+                    Section {
+                        let filteredTasks = section == .pending ? pendingTasks : completedTasks
+                        if filteredTasks.isEmpty {
+                            Text("No tasks available.")
+                        }
+                        
+                        ForEach(filteredTasks) { $task in
+                            TaskViewCell(task: $task)
+                        }
+                        .onDelete { indexSet in
+                            indexSet.forEach { index in
+                                let taskToDelete = filteredTasks[index]
+                                tasks = tasks.filter { $0.id != taskToDelete.id }
+                            }
+                        }
+                    } header: {
+                        Text(section.rawValue)
+                    }
+                }
+            }
+            VStack {
+                TextField("Task description...", text: $newTaskText)
+                    .textFieldStyle(.roundedBorder)
+                Button {
+                    tasks.append(Task(name: newTaskText))
+                    newTaskText = ""
+                } label: {
+                    Text("Add")
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            .padding()
         }
-        .padding()
+    }
+}
+
+struct TaskViewCell: View {
+    @Binding var task: Task
+    
+    var body: some View {
+        HStack {
+            Image(systemName: task.isCompleted ? "checkmark.square" : "square")
+                .onTapGesture {
+                    task.isCompleted.toggle()
+                }
+            Text(task.name)
+        }
     }
 }
 
